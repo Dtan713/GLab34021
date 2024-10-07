f59a465baa82c522549f4b653caf64be6d2aeb51
Dtan713 <s.donaldtan@gmail.com>
2024-10-07 12:21:09 -0400

    glab34021

---
+++ PythonOperators.py
@@ -0,0 +1,75 @@
+a = 32  # Initialize the value of a
+b = 6 # Initialize the value of b
+
+print('Addition of two numbers:', a + b) 
+print('Subtraction of two numbers:',a-b)  
+print('Multiplication of two numbers:',a*b)  
+print('Division of two numbers:',a/b)  
+print('Reminder of two numbers:',a%b)  
+print('Exponent of two numbers:',a**b)  
+print('Floor division of two numbers:',a//b)  
+
+MultipleOperators = 1 + 2 * 3 / 4.0
+
+print("Resultof MultipleOperators is: ", MultipleOperators)
+
+
+a = 32
+b = 6
+print('a=b:',a==b)
+print('a+=b:',a+b)
+print('a-=b:',a-b)
+print('a*=b:',a*b)
+print('a%=b:',a%b)
+print('a**=b:',a**b)
+print('a//=b:',a//b)
+
+j = 10 # j gets the value 10
+print('j is :',j)
+j = 5   
+k = j
+print("j is : ",j)
+print("k is : ",k)
+
+k = j = 10; # (k = (j = 10))
+print("j is : ",j)
+print("k is : ",k)
+
+
+a = 32     
+b = 6      
+print('Two numbers are equal or not:',a==b)  
+print('Two numbers are not equal or not:',a!=b)  
+print('a is less than or equal to b:',a<=b)  
+print('a is greater than or equal to b:',a>=b)  
+print('a is greater b:',a>b)  
+print('a is less than b:',a<b)  
+
+
+a = 5            
+print('Is this statement true?:',a > 3 and a < 5)  
+print('Any one statement is true?:',a > 3 or a < 5)  
+print('Each statement is true then return False and vice-versa:',(not(a > 3 and a < 5)))
+
+a = ["Rose", "Lotus"]
+b = ["Rose", "Lotus"]
+c = a
+
+print( a is c)
+print(a is not c)
+print(a is b)
+print(a is not b)
+print(a == b)
+print(a != b)
+
+x = ["Rose", "Lotus"]
+print(' Is Value Present?', "Rose" in x)
+print(' Is Value Present?', "Riya" not in x)
+x = 'Hello world'
+y = {1:'a',2:'b'}
+
+print('H' in x)
+print('hello' not in x)
+print(1 in y)
+print('a' in y)
+
